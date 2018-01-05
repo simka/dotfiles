eb225183deee3efc3bd654429a53445c8c90c901
maciej simka <maciej.simka@kissdigital.com>
2018-01-05 16:43:51 +0100

    fix display setup at work

--- scripts/scripts/xrandr.sh
+++ scripts/scripts/xrandr.sh
@@ -2,8 +2,8 @@
 
 if [[ $(xrandr | grep DP-1) ]]; then
   # work
-  xrandr --output DP-1 --auto --output DP-2 --auto --left-of DP-1
+  xrandr --output DP-1 --auto --primary --output DP-2 --auto --right-of DP-1
 else
   # home
-  xrandr --output HDMI-2 --auto
+  xrandr --output HDMI-2 --auto --primary
 fi
