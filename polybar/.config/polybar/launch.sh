e9b5f1e60d8521f6019674ea8479b13c62e250e3
maciej simka <maciej.simka@kissdigital.com>
2018-01-05 14:51:55 +0100

    add separate polybar bar for work

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-polybar bottom
+# Launch bar according to current env
+if [[ $(xrandr | grep DP-1) ]]; then
+    polybar work
+else
+    polybar default
+fi
 
-echo "Bars launched..."
+echo "polybar launched"
