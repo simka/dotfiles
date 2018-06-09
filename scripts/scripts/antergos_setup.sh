fc65b7ad340543795b107cb6382297d918c0b2d2
maciej simka <mcj.simka@gmail.com>
2018-06-09 12:11:51 +0200

    change antergos setup script

--- scripts/scripts/antergos_setup.sh
+++ scripts/scripts/antergos_setup.sh
@@ -1,23 +1,33 @@
+#!/bin/bash
+set -e
+
 echo 'installing pacaur'
 sudo pacman -S pacaur --noconfirm --needed
 echo 'done'
 
-echo 'installing xorg etc.'
-pacaur -S xorg-server xorg-apps xorg-xinit xterm xf86-video-intel i3 --noconfirm --needed
+echo 'installing xorg and i3'
+pacaur -S xorg-server xorg-apps xorg-xinit xterm xf86-video-intel i3
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 echo 'done'
 echo 'edit ~/.xinitrc and add exec i3 at the end'
 
 echo 'installing packages'
-pacaur -S audacity beets calibre chromium compton deadbeef deja-dup deluge dropbox dunst emacs exa feh firefox gimp gnucash heroku-cli htop httpie keepassxc kitty most nemo neovim networkmanager nm-applet newsboat polybar ranger redshift ripgrep rofi soulseekqt spotify stow tig tmux tree udiskie virtualbox virtualbox-host-modules-arch visual-studio-code xcape zsh
+pacaur -S audacity beets calibre chromium compton deadbeef deja-dup deluge dropbox dunst emacs exa feh firefox gimp gnucash heroku-cli htop httpie keepassxc kitty most nemo neovim networkmanager nm-applet newsboat polybar ranger redshift ripgrep rofi soulseekqt spotify stow tig tmux tree udiskie virtualbox virtualbox-host-modules-arch visual-studio-code-bin xcape zsh
 echo 'done'
 
 echo 'setting zsh as default shell'
 chsh -s $(which zsh)
 echo 'done'
 
+echo 'installing zplug'
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+echo 'done'
+
+echo 'installing z'
+git clone https://github.com/rupa/z.git ~/z
+echo 'done'
+
 echo 'symlinking dotfiles'
-stow coloschemes
 stow compton
 stow dunst
 stow fonts
