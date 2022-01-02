42ccf945e402a592c7cbf13c80d015ed44cbf219
maciej simka <mcj.simka@gmail.com>
2022-01-02 15:49:25 +0100

    Couple of adjustments on macOS

--- home.nix
+++ home.nix
@@ -3,15 +3,17 @@
 {
   nixpkgs.config.allowUnfree = true;
 
-  home.username = "simka";
-  home.homeDirectory = "/home/simka";
+  home.username = "maciejsimka";
+  home.homeDirectory = "/Users/maciejsimka/";
 
   home.packages = [
+    pkgs.antibody
+    pkgs.direnv
     pkgs.fd
     pkgs.imagemagick
+    pkgs.neovim
     pkgs.starship
     pkgs.ripgrep
-    pkgs.vscode
   ];
 
   home.stateVersion = "22.05";
@@ -28,12 +30,13 @@
   programs.htop.enable = true;
   programs.jq.enable = true;
   programs.lazygit.enable = true;
+  programs.nnn.enable = true;
 
   home.file.".nvm/default-packages".source = config.lib.file.mkOutOfStoreSymlink ./nvm/default-packages;
   home.file.".zshenv".source = config.lib.file.mkOutOfStoreSymlink ./zsh/zshenv;
   home.file.".zsh/.zshrc".source = config.lib.file.mkOutOfStoreSymlink ./zsh/zshrc;
+  home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink ./git/config;
   xdg.configFile."alacritty/alacritty.yml".source = config.lib.file.mkOutOfStoreSymlink ./alacritty/alacritty.yml;
-  xdg.configFile."/git/config".source = config.lib.file.mkOutOfStoreSymlink ./git/config;
   xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./nvim/init.lua;
   xdg.configFile."starship/starship.toml".source = config.lib.file.mkOutOfStoreSymlink ./starship/starship.toml;
 }
--- nvm/nvm.zsh
+++ nvm/nvm.zsh
@@ -1,1 +1,2 @@
-source /usr/share/nvm/init-nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
